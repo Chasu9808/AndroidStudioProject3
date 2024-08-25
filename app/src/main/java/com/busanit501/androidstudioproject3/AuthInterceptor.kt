6bacccf4e45fca73c80f28b54918dfde57ea961a
LeeHanSol <hslee4954@gmail.com>
2024-08-25 23:07:00 +0900

    JWT 토큰을 서버에서 받아 SharedPreferences에 저장하고, 이를 활용해 Retrofit을 통해 서버 요청 시 자동으로 헤더에 토큰을 추가. 로그인 액티비티 회원가입 버튼 에러 이슈

---
+++ app/src/main/java/com/busanit501/androidstudioproject3/AuthInterceptor.kt
@@ -0,0 +1,14 @@
+package com.busanit501.androidstudioproject3
+
+import com.busanit501.androidstudioproject3.App
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val req = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer " + (App.prefs.token ?: ""))
+            .build()
+        return chain.proceed(req)
+    }
+}
