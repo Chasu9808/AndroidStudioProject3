a8d9ca8c6837a1f6bf33cf66a0c65f1d75fb7f07
lcs <ggggggggg1948@gmail.com>
2024-09-06 03:08:20 +0900

    Activity 메인부분 수정완료 현재 마이페이지 회원탈퇴기능 토큰 기입하여 테스트 진행중 토큰 값이 현재 정상적으로 나오는 상태, 단 DB안에 계정을 삭제를 못함 내일 확인필요

--- app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/viewModel/LoginViewModel.kt
@@ -1,5 +1,3 @@
-package com.sylovestp.firebasetest.testspringrestapp.viewModel
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,13 +7,20 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
-    private val _loginResult = MutableLiveData<Boolean>()
-    val loginResult: LiveData<Boolean> get() = _loginResult
+    private val _loginUser = MutableLiveData<String?>()  // 로그인한 사용자 ID 또는 null
+    val loginUser: LiveData<String?> get() = _loginUser
 
     fun login(username: String, password: String) {
+        // 코루틴 내에서 suspend 함수를 호출
         viewModelScope.launch {
-            val success = loginRepository.login(username, password)
-            _loginResult.value = success
+            // loginRepository.login()은 Boolean을 반환하므로 성공 여부를 확인
+            val loginSuccessful = loginRepository.login(username, password)
+
+            if (loginSuccessful) {
+                _loginUser.postValue(username)  // 로그인 성공 시 사용자 이름을 저장
+            } else {
+                _loginUser.postValue(null)  // 로그인 실패 시 null 설정
+            }
         }
     }
 }
