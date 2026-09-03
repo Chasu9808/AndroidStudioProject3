f24a8c801d611eb85f51fbf05e5c7d551e98b028
LeeHanSol <hslee4954@gmail.com>
2026-09-03 23:38:55 +0900

    CAMERA 권한 기입, 이미지/비디오 각각 권한 주석처리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sylovestp.firebasetest.testspringrestapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0.2"
+        versionCode = 22
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
